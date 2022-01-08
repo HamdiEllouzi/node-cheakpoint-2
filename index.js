b8763da488bafb8c10870592f8779c53f6112bea
Nabil <Nabilkriden>
2022-01-08 15:48:33 +0100

    first code review

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use((req,res, next)=>{
     next(new Error('The web application is only available during working hours (Monday to Friday,  from 9 to 17)'));
 })
 app.get('/',(req,res)=>{
-    res.render('home page')
+    res.render('home-page')
 })
-app.get('/our_services',(req,res)=>{
-    res.render('Our Services')
+app.get('/our-services',(req,res)=>{
+    res.render('our-services')
 })
-app.get('/contact_us',(req,res)=>{
-    res.render('Contact us')
+app.get('/contact-us',(req,res)=>{
+    res.render('contact-us')
 })
 
 app.listen(port)
@@ -26,7 +26,7 @@ app.listen(port)
 const cheakDate = (date) => {
     const days = date.getDay()
     const hours = date.getHours()
-    if(days < 6 && days > 0){
+    if(days > 0){
         if(hours > 9 && hours < 17) return true
         return false
     }else return false
